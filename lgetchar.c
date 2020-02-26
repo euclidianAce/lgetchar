a484c7aebb87cec94e4b79beee2e90be29f5f15a
Corey Williamson <nerd1001001@gmail.com>
2020-02-25 22:33:18 -0600

    Added license and rockspec for eventual publishing to luarocks

--- lgetchar.c
+++ lgetchar.c
@@ -1,3 +1,16 @@
+/* lgetchar: Some Lua bindings to read character input from a terminal
+ *
+ * Copyright (C) 2020 Corey Williamson
+ *
+ * This file is part of lgetchar.
+ *
+ * lgetchar is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * lgetchar is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with lgetchar. If not, see http://www.gnu.org/licenses/.
+*/
+
 #include <sys/ioctl.h>
 #include <termios.h>
 
